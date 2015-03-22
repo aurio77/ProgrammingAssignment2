e2e2926862243e29c282f447ee8ee372c7bf56fa
aurio77 <dj7aurio@gmail.com>
2015-03-23 01:59:32 +1100

    Update 4 with extra comments

--- cachematrix.R
+++ cachematrix.R
@@ -13,22 +13,24 @@ makeCacheMatrix <- function(x = matrix()) {
         i = NULL
         # Intialise local i to be NULL
         
-        set = function(y) {
+        set <- function(y) {
                 x <<- y
                 i <<- NULL
         }
         # "<<-" is used to assign a value to an object in an environment 
         # that is different from the current environment
         
-        get = function() x
-        setinverse = function(inverse) i <<- inverse 
-        getinverse = function() i
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i 
         
         list(set = set, 
              get = get, 
              setinverse = setinverse, 
              getinverse = getinverse)
         # Returns list of functions used as input for cacheSolve
+        # setinverse: setting input inverse to i and returns inverse i
+        # getinverse: returns cached inverse i
         
 }
 
@@ -41,22 +43,23 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
 
         i <- x$getinverse()
-        # Set inverse matrix
+        # Set i to get the inverse matrix in makeCacheMatrix 
         
         if (!is.null(i)){
                 message("getting cached data")
                 return(i)
         }
         # If inverse already calculated, get from cache, output message, skip
-        # below steps and return the inverse
-        
-        # Otherwise get the inverse with the solve() function and store as below
+        # below steps and return the inverse i. Otherwise do the following.
         
         data <- x$get()
+        # get the matrix and store as "data"
         i <- solve(data, ...)
+        # get the inverse with the solve() function and store as i
         x$setinverse(i)
+        # remember the inverse, i.e cache it via setinverse in makeCacheMatrix
         
         i
-        # Return the inverse
+        # Return the inverse i
         
 }
